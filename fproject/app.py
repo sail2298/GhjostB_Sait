afa3fd7289e742dab2c61b075867cdccf61ad72a
rasso <rassomakhin05@gmail.com>
2024-05-31 12:00:31 +0300

    add fil

---
+++ fproject/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/profile', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # проверка логина и пароля
+        return 'Вы вошли в систему!'
+    else:
+        return render_template('authorization.html')
+
+
+
+@app.route('/authorization', methods=['GET', 'POST'])
+def form_authorization():
+    if request.method == 'POST':
+        Login = request.form.get('Login')
+        Password = request.form.get('Password')
+
+        db_lp = sqlite3.connect('login_password.db')
+        cursor_db = db_lp.cursor()
+        cursor_db.execute(('''SELECT password FROM passwords
+                                               WHERE login = '{}';
+                                               ''').format(Login))
+        pas = cursor_db.fetchall()
+
+        cursor_db.close()
+        try:
+            if pas[0][0] != Password:
+                return render_template('auth_bad.html')
+        except:
+            return render_template('auth_bad.html')
+
+        db_lp.close()
+        return render_template('profile.html')
+
+    return render_template('authorization.html')
+
+@app.route('/registration', methods=['GET', 'POST'])
+def form_registration():
+
+   if request.method == 'POST':
+       Login = request.form.get('Login')
+       Password = request.form.get('Password')
+
+       db_lp = sqlite3.connect('login_password.db')
+       cursor_db = db_lp.cursor()
+       sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(Login, Password)
+
+       cursor_db.execute(sql_insert)
+       db_lp.commit()
+
+       cursor_db.close()
+       db_lp.close()
+
+       return render_template('profile.html')
+
+   return render_template('registration.html')
+
+if __name__ == '__main__':
+    app.run()
+
